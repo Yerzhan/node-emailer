caf719cd3fafc7cc7652c9b9735dbd237b1a6bb8
sardieri <sardieri@gmail.com>
2018-07-21 15:27:09 +0300

    mongo DB integration

--- services/passport.js
+++ services/passport.js
@@ -2,15 +2,24 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
 passport.use(
     new GoogleStrategy({
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback'
         }, (accessToken, refreshToken, profile, done) => {
-            console.log('accessToken', accessToken);
-            console.log('refreshToken', refreshToken);
-            console.log('profile', profile);
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        // We already have record for user
+                    } else {
+                        new User({ googleId: profile.id }).save();
+                    }
+                });
         }
     )
 );
