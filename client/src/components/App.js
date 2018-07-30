a31ac78a40102bb43fd03e6065b95be7e8be0722
sardieri <sardieri@gmail.com>
2018-07-30 21:52:35 +0300

    added redux

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      Hi there!
+    </div>
+  );
+}
+
+export default App;
