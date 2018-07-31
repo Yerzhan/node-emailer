2430ad7fd774c8a81d6c6d071d6031199141cdf1
sardieri <sardieri@gmail.com>
2018-07-31 23:24:35 +0300

    header implemented

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
+import Landing from './Landing';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
 
 
 class App extends Component {
---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>
+				Emailer!
+			</h1>
+
+			Collect feedback for your surveys
+		</div>
+	);
+};
+
+export default Landing;
