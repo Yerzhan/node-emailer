d810d9ff77143518b68948875e0b8ffd94c1a527
sardieri <sardieri@gmail.com>
2018-08-06 00:14:40 +0300

    finished surveys on backend

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,12 +3,16 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Survey  = mongoose.model('surveys');
 const Mailer = require('../services/Mailer');
-const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const surveyTemplate = require('../services/emailTemplates');
 
 module.exports = app => {
 
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    });
+
     app.post(
-        '/api/surveys', requireLogin, requireCredits, (req, res) => {
+        '/api/surveys', requireLogin, requireCredits, async (req, res) => {
             const {title, subject, body, recipients} = req.body;
 
             const survey = new Survey({
@@ -21,6 +25,17 @@ module.exports = app => {
             });
 
             const mailer = new Mailer(survey, surveyTemplate(survey));
+
+            try {
+                await mailer.send();
+                await survey.save();
+                req.user.credits -= 1;
+                const user = await req.user.save();
+
+                res.send(user);
+            } catch (err) {
+                res.status(422).send(err);
+            }
 
         }
     );
